46a660c37080db97aa234a755a8bcf953961ebe8
MizanRbf <mizanrbf@gmail.com>
2025-11-17 08:04:32 +0600

    all 8 problems initialized

---
+++ src/solution.ts
@@ -0,0 +1,35 @@
+// All comment and console.log should be removed
+// Problem-1
+const formatValue = (value: string | number | boolean) => {};
+
+// Problem-2
+const getLength = (value: string | []) => {};
+
+// Problem-3
+class Person {
+  name: string;
+  age: number;
+  constructor(name: string, age: number) {
+    (this.name = name), (this.age = age);
+  }
+}
+
+// Problem-4
+const filterByRating = (items: []) => {};
+
+// Problem-5
+const filterActiveUsers = (items: []) => {};
+
+// Problem-6
+interface Book {
+  title: string;
+  author: string;
+  publishedYear: number;
+  isAvailable: boolean;
+}
+
+// Problem-7
+const getUniqueValues = (value: []) => {};
+
+// Problem-8
+const calculateTotalPrice = (value: []) => {};
